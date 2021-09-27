9196b763b091c433ffb32f3e1c1b7726f42c7ef7
STARK <89933807+XD-STARK@users.noreply.github.com>
2021-09-27 12:19:13 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,4 @@
 @echo off
-TITLE phantom Robot
-rem This next line removes any fban csv files if they exist in root when bot restarts. 
-del *.csv
-py -3.7 --version
-IF "%ERRORLEVEL%" == "0" (
-    py -3.7 -m tg_bot
-) ELSE (
-    py -m tg_bot
-)
-
-pause
+TITLE LunaRobot
+:: Enables virtual env mode and then starts LunaRobot
+env\scripts\activate.bat && py -m LunaRobot
